7e2b0021ddee961d528e5c1d2eeadd92d05a4ee2
Alabi12 <wolo22001@gmail.com>
2023-06-23 00:09:04 +0000

    Add redux ant redux toolkit and the necessary files for adding to cart

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function RootLayout() {
   return (
     <div>
-      <NavBar />
+      <Provider store={store}>
+        <NavBar />
 
       <main>
         <Outlet />
       </main>
+      </Provider>
+      
     </div>
   );
 }
